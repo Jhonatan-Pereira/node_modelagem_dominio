48b2d79e5d29f0df856c9396849029a4dc1facee
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2023-12-03 17:43:45 -0300

    feature: validações nome pessoa

--- src/core/shared/NomePessoa.ts
+++ src/core/shared/NomePessoa.ts
@@ -7,8 +7,14 @@ export default class NomePessoa {
     constructor(nome: string) {
         this.nome = nome.trim()
 
-        Validador.combinar(
-            Validador.naoVazio(this.nome, Erros.NOME_VAZIO)
+        const erros = Validador.combinar(
+            Validador.naoVazio(this.nome, Erros.NOME_VAZIO),
+            Validador.tamanhoMaiorQue(this.nome, 4, Erros.NOME_PEQUENO),
+            Validador.tamanhoMenorQue(this.nome, 121, Erros.NOME_GRANDE),
+            Validador.naoVazio(this.nome.split(' ')[1], Erros.NOME_SEM_SOBRENOME),
+            Validador.regex(this.nome, /^[a-zA-ZÀ-ú\s]$/, Erros.NOME_CARACTERES_INVALIDOS)
         )
+
+        if(erros) throw new Error(erros.join(','))
     }
 }
---
+++ test/core/shared/NomePessoa.test.ts
@@ -0,0 +1,23 @@
+import Erros from "@/core/constants/Erros"
+import NomePessoa from "@/core/shared/NomePessoa"
+
+test('Deve lançar erro ao tentar criar nome vazio', () => {
+    expect(() => new NomePessoa('')).toThrowError(Erros.NOME_VAZIO)
+})
+
+test('Deve lançar erro ao tentar criar nome menor que 4 caracteres', () => {
+    expect(() => new NomePessoa('Li Z')).toThrowError(Erros.NOME_PEQUENO)
+})
+
+test('Deve lançar erro ao tentar criar nome maior que 120 caracteres', () => {
+    const nomeGigante = 'Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon'
+    expect(() => new NomePessoa(nomeGigante)).toThrowError(Erros.NOME_GRANDE)
+})
+
+test('Deve lançar erro ao tentar criar nome sem sobrenome', () => {
+    expect(() => new NomePessoa("Guilherme")).toThrowError(Erros.NOME_SEM_SOBRENOME)
+})
+
+test('Deve lançar erro ao tentar criar nome com caracteres especiais', () => {
+    expect(() => new NomePessoa("João @OOOJoao")).toThrowError(Erros.NOME_CARACTERES_INVALIDOS)
+})
