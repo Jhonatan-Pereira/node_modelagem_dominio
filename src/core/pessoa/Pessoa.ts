ea7b9f9a3c8e75d8a657fb5a9604085782a380b8
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2023-12-29 23:49:14 -0300

    feature: criado abstract class entidade

--- src/core/pessoa/Pessoa.ts
+++ src/core/pessoa/Pessoa.ts
@@ -1,26 +1,21 @@
 import Cpf from "../shared/Cpf";
-import Id from "../shared/Id";
+import Entidade, { EntidadeProps } from "../shared/Entidade";
 import NomePessoa from "../shared/NomePessoa";
 
-export interface PessoaProps {
-    id?: string
+export interface PessoaProps extends EntidadeProps {
     nome?: string
     cpf?: string
 
 }
 
-export default class Pessoa {
-    readonly props: PessoaProps
-    readonly id: Id
+export default class Pessoa extends Entidade<Pessoa, PessoaProps> {
     readonly nome: NomePessoa
     readonly cpf: Cpf
     
     constructor(props: PessoaProps) {
-        this.id = new Id(props.id)
+        super(props)
         this.nome = new NomePessoa(props.nome)
         this.cpf = new Cpf(props.cpf)
-
-        this.props = { ...props, id: this.id.valor }
     }
 
     clone(newProps: PessoaProps) {
